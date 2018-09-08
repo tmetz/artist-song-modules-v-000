58d71037b03af7c84f357e4631e4d068682318e6
Tammy Metz <tammy@tammymetz.com>
2018-09-08 22:24:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
 
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
@@ -26,10 +27,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
