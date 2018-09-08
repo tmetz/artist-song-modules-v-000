a79e5437b4e75907d5a678e8cfbeec0853d91ae9
Tammy Metz <tammy@tammymetz.com>
2018-09-08 22:14:12 +0000

    automatically backed up by learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,19 @@
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    # def self.count
+    #   @@artists.count
+    # end
+    # 
+    # def self.reset_all
+    #   @@songs.clear
+    # end
+
+    def count
+      self.all.count
+    end
+  end
+end
