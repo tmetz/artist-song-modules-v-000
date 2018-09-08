caacb3533da84ee7730ba10a019e70509c50c453
Tammy Metz <tammy@tammymetz.com>
2018-09-08 22:13:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 class Artist
+
+  extend Memorable::ClassMethods
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -17,14 +20,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
